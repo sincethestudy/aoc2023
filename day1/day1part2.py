e80b107bb63369aa177ecc724fa4712a4e1ef42f
Brian Machado <bmachado@live.ca>
2023-12-01 14:47:34 -0500

    done day 1

--- day1/day1part2.py
+++ day1/day1part2.py
@@ -1,14 +1,43 @@
+## the strat for this one thats more interesting to do is to reverse the string and reverse the words we are checking, and then run the same algorithm for both first and last
+
 with open('input.txt', 'r') as file:
     total_sum = 0
+    words = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
     for i, line in enumerate(file):
         print(line)
-        digits = [num for num in line if num.isdigit()]
-        print(digits)
+        found = False
+        first = ''
+        last = ''
+        for j in range(len(line)):
+            if found:
+                break
+            for word in words:
+                if line.startswith(word, j):
+                    first = words.index(word) + 1
+                    found = True
+                    break
+            if not found and line[j].isdigit():
+                first = line[j]
+                break
 
-        if len(digits) == 1:
-            total_sum += int(str(digits[0]) + str(digits[0]))
+        found = False
+        for j in range(len(line) - 1, -1, -1):
+            if found:
+                break
+            for word in words:
+                if line.startswith(word, j):
+                    last = words.index(word) + 1
+                    found = True
+                    break
+            if not found and line[j].isdigit():
+                last = line[j]
+                break
 
-        else:
-            total_sum += int(str(digits[0]) + str(digits[-1]))
+        print(int(str(first) + str(last)))
+
+        total_sum += int(str(first) + str(last))
+            
+
+
 print(total_sum)
 
