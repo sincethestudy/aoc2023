c191ee1dc80807451aa5b59366a5f8d0fabef4f4
Brian Machado <bmachado@live.ca>
2023-12-11 23:11:00 -0500

    dfD

--- day7/day7part2.py
+++ day7/day7part2.py
@@ -2,10 +2,14 @@
 data = open('input.txt').read().splitlines()
 hands, bids = zip(*[line.split() for line in data])
 
-rank = 'AKQJT98765432'
+rank = 'AKQT98765432J'
 
 def tuplize(hand):
-    return tuple(sorted([hand.count(x) for x in set(hand)], reverse=True))
+    h = []
+    for c in rank:
+        h.append(tuple(sorted([hand.replace('J', c).count(x) for x in set(hand.replace('J', c))], reverse=True)))
+
+    return list(sorted(h, reverse=True))[0]
 
 def sort_me(hand):
     return (tuplize(hand), [-rank.index(x) for x in hand])
